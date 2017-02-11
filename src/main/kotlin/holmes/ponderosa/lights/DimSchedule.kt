a4fec2e4f9fa4331f4442ba1e7f338392c59837e
Jason Holmes <holmes@squareup.com>
2017-02-10 17:36:13 -0800

    AutoDim testing and tweaks

--- src/main/kotlin/holmes/ponderosa/lights/DimSchedule.kt
+++ src/main/kotlin/holmes/ponderosa/lights/DimSchedule.kt
@@ -1,25 +1,70 @@
 package holmes.ponderosa.lights
 
+import java.time.Duration
 import java.time.LocalTime
 
+/**
+ * Represents a room with various light levels defined by TimeFrame(s). TimeFrame(s) are defined by
+ * their endTime(s) and are stored consecutively in the List.
+ */
 data class DimSchedule(val deviceId: Int, val timeFrames: List<TimeFrame>, val now: () -> LocalTime) {
   fun autoDim(currentValue: Int): Int {
     if (currentValue == 0) {
       return -1
     }
 
-    if (isInFirstFrame) {
-      return timeFrames.first().lowLevel
+    // If the lights aren't within this level, don't bother doing anything.
+    val currentFrame = currentFrame
+    if (currentValue >= currentFrame.lowLevel) {
+      return -1
     }
 
-    return -1
+    val calculatedLevel = calculateLightLevel()
+    val allowed = Math.abs(calculatedLevel - currentValue) <= 3
+
+    return if (allowed) calculatedLevel else -1
   }
 
   fun toggleLights(currentValue: Int): Int {
     return -1
   }
 
+  private fun calculateLightLevel(): Int {
+    // Always use lowLevel in the morning.
+    if (isInFirstFrame) {
+      return timeFrames.first().lowLevel
+    }
+
+    val startValue = previousFrame.lowLevel
+    val endValue = currentFrame.lowLevel
+
+    val startTime = previousFrame.endTime
+    val endTime = currentFrame.endTime
+    val nowTime = now()
+
+    val frameLength = Duration.between(startTime, endTime)
+    val inset = Duration.between(startTime, nowTime)
+    val ratio: Double = (inset.toMinutes().toDouble() / frameLength.toMinutes().toDouble())
+    val adjustedRatio = (Math.abs(startValue - endValue) * ratio).toInt()
+
+    var calculatedLevel: Int
+    if (startValue > endValue) {
+      calculatedLevel = Math.abs(adjustedRatio - startValue)
+      calculatedLevel = Math.max(calculatedLevel, endValue)
+    } else {
+      calculatedLevel = adjustedRatio + startValue
+      calculatedLevel = Math.max(calculatedLevel, startValue)
+    }
+
+    return calculatedLevel
+  }
 
   private val isInFirstFrame: Boolean
-  get() = timeFrames.get(0).contains(now())
+    get() = timeFrames.get(0).contains(now())
+
+  internal val previousFrame: TimeFrame
+    get() = timeFrames.last { !it.contains(now()) }
+
+  internal val currentFrame: TimeFrame
+    get() = timeFrames.first { it.contains(now()) }
 }
--- src/test/kotlin/holmes/ponderosa/lights/DimScheduleUnitTest.kt
+++ src/test/kotlin/holmes/ponderosa/lights/DimScheduleUnitTest.kt
@@ -10,22 +10,84 @@ class DimScheduleUnitTest {
   lateinit var now: LocalTime
 
   @Before fun setUp() {
-    val now = LocalTime.of(0, 0)
+    val midnight = LocalTime.of(0, 0)
+    val timeFrames = listOf(
+      TimeFrame(midnight.plusHours(7), 1, 35),
+      TimeFrame(midnight.plusHours(8), 1, 35),
+      TimeFrame(midnight.plusHours(13), 30, 85),
+      TimeFrame(midnight.plusHours(23).plusMinutes(59), 1, 35)
+    )
 
-    val timeFrames = listOf(1, 2, 3, 4)
-        .map { TimeFrame(now.plusHours((it * 5).toLong()), it * 10, it * 20) }
-
-    dimSchedule = DimSchedule(22, timeFrames, { now })
+    dimSchedule = DimSchedule(22, timeFrames) { now }
   }
 
   @Test fun testAutoDimLightIsOff() {
     assertThat(dimSchedule.autoDim(0)).isEqualTo(-1)
   }
 
-  @Test fun testAutoDimLightnFirstFrameIsLowLevel() {
+  @Test fun testDoNotAutoDimLightInFirstFrame() {
     val firstFrame = dimSchedule.timeFrames[0]
 
     now = firstFrame.endTime.minusHours(1)
-    assertThat(dimSchedule.autoDim(10)).isEqualTo(firstFrame.lowLevel)
+    assertThat(dimSchedule.autoDim(10)).isEqualTo(-1)
   }
+
+  @Test fun testAutoDimLightAtVeryLowLevelDoNothing() {
+    val currentFrame = dimSchedule.timeFrames[1]
+
+    now = currentFrame.endTime.minusHours(1)
+    assertThat(dimSchedule.autoDim(1)).isEqualTo(-1)
+  }
+
+  @Test fun testAutoDimOverHighLevelDoNothing() {
+    val currentFrame = dimSchedule.timeFrames[2]
+
+    now = currentFrame.endTime.minusHours(1)
+    assertThat(dimSchedule.autoDim(88)).isEqualTo(-1)
+  }
+
+  @Test fun testAutoDimLightNearCurrentLevelDims() {
+    val testFrame = dimSchedule.timeFrames[2]
+    val currentValue = 27
+
+    now = testFrame.endTime.minusMinutes(10)
+    assertThat(dimSchedule.autoDim(currentValue)).isEqualTo(29)
+  }
+
+  @Test fun testAutoDimLighNottNearCurrentDoNothing() {
+    val testFrame = dimSchedule.timeFrames[2]
+    val currentValue = testFrame.lowLevel + 1
+
+    now = testFrame.endTime.minusMinutes(10)
+    assertThat(dimSchedule.autoDim(currentValue)).isEqualTo(-1)
+  }
+
+  @Test fun testAutoDimForReals() {
+    val currentFrame = dimSchedule.timeFrames[2]
+
+    now = currentFrame.endTime.minusMinutes(10)
+    assertThat(dimSchedule.autoDim(28)).isEqualTo(29)
+  }
+
+  @Test fun testAutoDimForSameValue() {
+    val currentFrame = dimSchedule.timeFrames[2]
+
+    now = currentFrame.endTime.minusMinutes(10)
+    assertThat(dimSchedule.autoDim(29)).isEqualTo(29)
+  }
+
+  @Test fun testPreviousFrame() {
+    val previousFrame = dimSchedule.timeFrames[1]
+    val testingFrame = dimSchedule.timeFrames[2]
+
+    now = testingFrame.endTime.minusHours(1)
+    assertThat(dimSchedule.previousFrame).isSameAs(previousFrame)
+  }
+
+  @Test fun testCurrentFrame() {
+    val testingFrame = dimSchedule.timeFrames[2]
+    now = testingFrame.endTime.minusHours(1)
+    assertThat(dimSchedule.currentFrame).isSameAs(testingFrame)
+  }
+
 }
