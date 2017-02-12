edd7d1aa782251b2bbae660b58b68baa6b3fce85
Jason Holmes <holmes@squareup.com>
2017-02-12 10:30:30 -0800

    OMG it works. And damned if I can figure out how to get Dagger to work

---
+++ src/main/kotlin/holmes/ponderosa/lights/LightsRequest.kt
@@ -0,0 +1,67 @@
+package holmes.ponderosa.lights
+
+import java.time.LocalTime
+
+class LightsRequest(val twilight: Twilight, val now: (() -> LocalTime)) {
+
+  fun autoDim(deviceId: Int, currentValue: Int): DimSchedule.AutoDimResult {
+    return getDimSchedule(deviceId).autoDim(currentValue)
+  }
+
+  fun toggleLights(deviceId: Int, currentValue: Int): Int {
+    return getDimSchedule(deviceId).toggleLights(currentValue)
+  }
+
+  fun getDimSchedule(deviceId: Int): DimSchedule {
+    val timeFrames: List<TimeFrame>
+
+    when (deviceId) {
+      22 -> { // Family Room
+        timeFrames = listOf(
+            TimeFrame(twilight.twilightBegin(), 1, 35),
+            TimeFrame(twilight.sunrise(), 1, 35),
+            TimeFrame(twilight.solarNoon(), 30, 85),
+            TimeFrame(twilight.twilightEnd(), 10, 35),
+            TimeFrame(LocalTime.MAX, 1, 35)
+        )
+      }
+      28 -> { // Nursery
+        timeFrames = listOf(
+            TimeFrame(twilight.twilightBegin(), 1, 35),
+            TimeFrame(twilight.sunrise(), 1, 35),
+            TimeFrame(twilight.solarNoon(), 30, 85),
+            TimeFrame(LocalTime.MAX, 1, 35)
+        )
+      }
+      25 -> { // Guest Bath
+        timeFrames = listOf(
+            TimeFrame(twilight.twilightBegin(), 5, 35),
+            TimeFrame(twilight.solarNoon(), 35, 85),
+            TimeFrame(twilight.twilightEnd(), 20, 35),
+            TimeFrame(LocalTime.of(23, 59), 5, 35)
+        )
+      }
+      19 -> { // Kitchen
+        timeFrames = listOf(
+            TimeFrame(twilight.twilightBegin(), 10, 35),
+            TimeFrame(twilight.solarNoon(), 35, 85),
+            TimeFrame(twilight.twilightEnd(), 25, 35),
+            TimeFrame(LocalTime.of(23, 59), 18, 35)
+        )
+      }
+      15 -> { // Dining Room
+        timeFrames = listOf(
+            TimeFrame(twilight.twilightBegin(), 5, 35),
+            TimeFrame(twilight.solarNoon(), 30, 85),
+            TimeFrame(twilight.twilightEnd(), 15, 60),
+            TimeFrame(LocalTime.of(23, 59), 5, 35)
+        )
+      }
+      else -> {
+        throw IllegalArgumentException("Unknown deviceId: $deviceId")
+      }
+    }
+
+    return DimSchedule(deviceId, timeFrames, now)
+  }
+}
