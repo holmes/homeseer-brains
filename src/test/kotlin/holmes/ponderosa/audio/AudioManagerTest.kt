a5cfac11d9e6cd99812a5157eaf8c0ef479ac89d
Jason Holmes <holmes@squareup.com>
2017-02-15 18:54:43 -0800

    Fix AudioManager tests

--- src/test/kotlin/holmes/ponderosa/audio/AudioManagerTest.kt
+++ src/test/kotlin/holmes/ponderosa/audio/AudioManagerTest.kt
@@ -1,52 +1,54 @@
 package holmes.ponderosa.audio
 
+import com.google.common.truth.Truth.assertThat
+import com.nhaarman.mockito_kotlin.any
+import com.nhaarman.mockito_kotlin.doAnswer
+import com.nhaarman.mockito_kotlin.mock
 import org.junit.Before
 import org.junit.Test
-import java.io.FileOutputStream
-import java.io.OutputStream
+import java.io.ByteArrayOutputStream
 
 class AudioManagerTest {
-  lateinit var outputStream: OutputStream
+  lateinit var outputStream: ByteArrayOutputStream
   lateinit var russoundCommands: RussoundCommands
   lateinit var audioManager: AudioManager
 
   @Before fun setUp() {
-    outputStream = FileOutputStream("/dev/cu.usbserial")
-//    russoundCommands = mock<RussoundCommands> {
-//      on { turnOn(any()) }
-//          .doAnswer {
-//            val zone = it.arguments[0] as RussoundCommands.Zone
-//            "Zone${zone.zoneId}On".toByteArray()
-//          }
-//
-//      on { turnOff(any()) }
-//          .doAnswer {
-//            val zone = it.arguments[0] as RussoundCommands.Zone
-//            "Zone${zone.zoneId}Off".toByteArray()
-//          }
-//
-//      on { listen(any(), any()) }
-//          .doAnswer {
-//            val source = it.arguments[0] as RussoundCommands.Source
-//            val zone = it.arguments[1] as RussoundCommands.Zone
-//            "Zone${zone.zoneId}Source${source.sourceId}".toByteArray()
-//          }
-//    }
+    russoundCommands = mock<RussoundCommands> {
+      on { turnOn(any()) }
+          .doAnswer {
+            val zone = it.arguments[0] as RussoundCommands.Zone
+            "Zone${zone.zoneId}On".toByteArray()
+          }
 
-    russoundCommands = RussoundCommands()
+      on { turnOff(any()) }
+          .doAnswer {
+            val zone = it.arguments[0] as RussoundCommands.Zone
+            "Zone${zone.zoneId}Off".toByteArray()
+          }
+
+      on { listen(any(), any()) }
+          .doAnswer {
+            val source = it.arguments[0] as RussoundCommands.Source
+            val zone = it.arguments[1] as RussoundCommands.Zone
+            "Zone${zone.zoneId}Source${source.sourceId}".toByteArray()
+          }
+    }
+
+    outputStream = ByteArrayOutputStream()
     audioManager = AudioManager(russoundCommands, outputStream)
   }
 
   @Test fun powerOnTurnsOn() {
     val expected = "Zone0On".toByteArray()
     audioManager.power(RussoundCommands.Zone(0, "Place"), true)
-//    assertThat(outputStream.toByteArray()).isEqualTo(expected)
+    assertThat(outputStream.toByteArray()).isEqualTo(expected)
   }
 
   @Test fun powerOffTurnsOff() {
     val expected = "Zone1Off".toByteArray()
     audioManager.power(RussoundCommands.Zone(1, "Place"), false)
-//    assertThat(outputStream.toByteArray()).isEqualTo(expected)
+    assertThat(outputStream.toByteArray()).isEqualTo(expected)
   }
 
   @Test fun changeSourceTurnsZoneOnAndChangesSource() {
@@ -54,9 +56,7 @@ class AudioManagerTest {
     val source = RussoundCommands.Source(0, "Source")
     val expected = "Zone${zone.zoneId}OnZone${zone.zoneId}Source${source.sourceId}".toByteArray()
 
-    audioManager.power(zone, false)
-//    audioManager.getStatus(RussoundCommands.Zone(1, "Place"))
-//    audioManager.changeSource(source, zone)
-//    assertThat(outputStream.toByteArray()).isEqualTo(expected)
+    audioManager.changeSource(source, zone)
+    assertThat(outputStream.toByteArray()).isEqualTo(expected)
   }
 }
