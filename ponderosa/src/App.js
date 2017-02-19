8d32c676ceef3fadfb40f42f7e9a112bdddfefdf
Jason Holmes <holmes@squareup.com>
2017-02-18 19:53:55 -0800

    Making network calls

--- ponderosa/src/App.js
+++ ponderosa/src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {Component, PropTypes} from 'react';
 import './App.css';
 
 import Slider from 'rc-slider';
@@ -6,30 +6,31 @@ import 'rc-slider/assets/index.css';
 
 import ToggleButton from 'react-toggle-button'
 
+import "whatwg-fetch"
 
 let sources = {
-  sources: [{name: "TV", sourceId: 0}, {name: "Chromecast", sourceId: 1},]
+  sources: [{name: "TV", sourceId: 1}, {name: "Chromecast", sourceId: 2},]
 };
 
 let zoneInformation = {
   zones: [{
-    zone: {name: "Family Room", zoneId: "0"},
-    source: {name: "TV", sourceId: "0"},
+    zone: {name: "Family Room", zoneId: "1"},
+    source: {name: "TV", sourceId: "1"},
     power: "true",
     volume: "35"
   }, {
-    zone: {name: "Kitchen", zoneId: "1"},
-    source: {name: "Chromecast", sourceId: "1"},
+    zone: {name: "Kitchen", zoneId: "2"},
+    source: {name: "Chromecast", sourceId: "2"},
     power: "true",
     volume: "30"
   }, {
-    zone: {name: "Outside", zoneId: "2"},
-    source: {name: "Chromecast", sourceId: "1"},
+    zone: {name: "Outside", zoneId: "3"},
+    source: {name: "Chromecast", sourceId: "2"},
     power: "false",
     volume: "65"
   }, {
-    zone: {name: "Master", zoneId: "3"},
-    source: {name: "Chromecast", sourceId: "1"},
+    zone: {name: "Master", zoneId: "4"},
+    source: {name: "Chromecast", sourceId: "2"},
     power: "false",
     volume: "22"
   }]
@@ -53,34 +54,84 @@ class ZoneInformation extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      source: props.zoneInfo.source.sourceId
+      zone: props.zoneInfo.zone,
+      sourceId: props.zoneInfo.source.sourceId,
+      volume: parseInt(props.zoneInfo.volume, 10),
+      power: props.zoneInfo.power === "true"
     }
   }
 
+  sourceChanged(sourceId) {
+    console.log(sourceId);
+    const url = `http://192.168.100.5:8080/ponderosa/api/audio/${this.props.zoneInfo.zone.zoneId}/source/${sourceId}`;
+
+    const thing = this;
+    fetch(url, {
+      method: "POST", mode: 'no-cors'
+    }).then(function () {
+      thing.setState({sourceId: sourceId})
+    });
+  }
+
+  volumeChanged(volume) {
+    const url = `http://192.168.100.5:8080/ponderosa/api/audio/${this.props.zoneInfo.zone.zoneId}/volume/${volume}`;
+
+    const thing = this;
+    fetch(url, {
+      method: "POST", mode: 'no-cors'
+    }).then(function () {
+      thing.setState({volume: volume})
+    });
+  }
+
+  powerToggled(originalPower) {
+    const power = !originalPower;
+    const url = `http://192.168.100.5:8080/ponderosa/api/audio/${this.props.zoneInfo.zone.zoneId}/power/${power}`;
+
+    const thing = this;
+    fetch(url, {
+      method: "POST", mode: 'no-cors'
+    }).then(function () {
+      thing.setState({power: power})
+    });
+  }
+
   render() {
-    const volumeLevel = parseInt(this.props.zoneInfo.volume, 10);
-    const powerValue = this.props.zoneInfo.power === "true";
+    const volumeLevel = this.state.volume;
+    const powerValue = this.state.power;
 
     return (
         <div>
           <h1>Zone: {this.props.zoneInfo.zone.name}</h1>
           <p>Source:
-            <select value={this.state.source} readOnly="true">
+            <select value={this.state.sourceId} onChange={(event) => { this.sourceChanged(event.target.value) }}>
               {sources.sources.map(source => {
                 return (
                     <option key={source.sourceId} value={source.sourceId}>{source.name}</option>)
               })}
             </select>
           </p>
-          <p>Volume: {this.props.zoneInfo.volume}</p>
-          <Slider defaultValue={volumeLevel}/>
-          <ToggleButton
-              value={powerValue} onToggle={(value) => {
-                self.setState({ value: !value})
-          }} />
+
+          <p>Volume: {volumeLevel}</p>
+          <Slider defaultValue={volumeLevel} onAfterChange={(value) => { this.volumeChanged(value) }}/>
+
+          <ToggleButton value={powerValue} onToggle={(value) => { this.powerToggled(value) }}/>
         </div>
     )
   };
 }
+
+ZoneInformation.propTypes = {
+  zoneInfo: PropTypes.shape({
+    zone: PropTypes.shape({
+      name: PropTypes.string.isRequired, zoneId: PropTypes.string.isRequired
+    }), //
+    source: PropTypes.shape({
+      name: PropTypes.string.isRequired, sourceId: PropTypes.string.isRequired
+    }), //
+    volume: PropTypes.string.isRequired, //
+    power: PropTypes.string.isRequired
+  })
+};
 
 export default App;
