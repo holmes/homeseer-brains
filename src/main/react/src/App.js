c2c6329f63c2330847a531f31ef2762777d095ed
Jason Holmes <holmes@squareup.com>
2017-02-22 14:15:50 -0800

    Break out SourceSelector

--- src/main/react/src/App.js
+++ src/main/react/src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
     super(props);
 
     let remoteHost; // It's just so much easier to do it this way than inject it properly.
-    if ( process.env.NODE_ENV === "development") {
+    if (process.env.NODE_ENV === "development") {
       remoteHost = "http://localhost:4567/";
     } else {
       remoteHost = "http://192.168.100.5:8080/ponderosa/"
@@ -64,6 +64,47 @@ class App extends Component {
   }
 }
 
+class SourceSelector extends React.Component {
+  sourceName(sourceId) {
+    if (sourceId === undefined || isNaN(sourceId)) {
+      return ""
+    }
+
+    for (let s of this.props.sources) {
+      // eslint-disable-next-line
+      if (s.sourceId === sourceId) {
+        return s.name
+      }
+    }
+  }
+
+  render() {
+    let id = this.props.zoneId + "SourceSelector";
+    return (
+        <DropdownButton
+            id={id}
+            className="source-selector"
+            title={this.sourceName(this.props.selectedSourceId)}
+            value={this.props.selectedSourceId}
+            onSelect={(eventKey) => {
+              this.props.sourceChanged(eventKey)
+            }}
+        >
+          {this.props.sources.map(source => {
+            return (
+                <MenuItem
+                    active={source.sourceId === this.props.selectedSourceId}
+                    eventKey={source.sourceId}
+                    key={source.sourceId}
+                    value={source.sourceId}>
+                  {source.name}
+                </MenuItem>)
+          })}
+        </DropdownButton>
+    );
+  }
+}
+
 // TODO I think it's time to move callbacks to App.
 class ZoneInformation extends React.Component {
   constructor(props) {
@@ -79,34 +120,15 @@ class ZoneInformation extends React.Component {
       sourceId: sourceId,
       volume: volume,
       power: power,
-
-      baseUrl: props.baseUrl,
-      sources: props.sources
     };
 
-    this.volumeUp = this.volumeUp.bind(this)
-    this.volumeDown = this.volumeDown.bind(this)
-  }
-
-  source(sourceId) {
-    for (let s of this.state.sources) {
-      // eslint-disable-next-line
-      if (s.sourceId == sourceId) {
-        return s
-      }
-    }
-  }
-
-  sourceName() {
-    if (this.state.sourceId === undefined || isNaN(this.state.sourceId)) {
-      return ""
-    } else {
-      return this.source(this.state.sourceId).name
-    }
+    this.volumeUp = this.volumeUp.bind(this);
+    this.volumeDown = this.volumeDown.bind(this);
+    this.sourceChanged = this.sourceChanged.bind(this);
   }
 
   sourceChanged(sourceId) {
-    const url = `${this.state.baseUrl}api/audio/${this.state.zone.zoneId}/source/${sourceId}`;
+    const url = `${this.props.baseUrl}api/audio/${this.state.zone.zoneId}/source/${sourceId}`;
 
     const thing = this;
     fetch(url, {
@@ -117,7 +139,7 @@ class ZoneInformation extends React.Component {
   }
 
   volumeChanged(volume) {
-    const url = `${this.state.baseUrl}api/audio/${this.state.zone.zoneId}/volume/${volume}`;
+    const url = `${this.props.baseUrl}api/audio/${this.state.zone.zoneId}/volume/${volume}`;
 
     const thing = this;
     fetch(url, {
@@ -128,7 +150,7 @@ class ZoneInformation extends React.Component {
   }
 
   volumeUp() {
-    const url = `${this.state.baseUrl}api/audio/${this.state.zone.zoneId}/volume/up`;
+    const url = `${this.props.baseUrl}api/audio/${this.state.zone.zoneId}/volume/up`;
 
     const thing = this;
     fetch(url, {
@@ -141,7 +163,7 @@ class ZoneInformation extends React.Component {
   }
 
   volumeDown() {
-    const url = `${this.state.baseUrl}api/audio/${this.state.zone.zoneId}/volume/down`;
+    const url = `${this.props.baseUrl}api/audio/${this.state.zone.zoneId}/volume/down`;
 
     const thing = this;
     fetch(url, {
@@ -155,7 +177,7 @@ class ZoneInformation extends React.Component {
 
   powerToggled(originalPower) {
     const power = !originalPower;
-    const url = `${this.state.baseUrl}api/audio/${this.state.zone.zoneId}/power/${power}`;
+    const url = `${this.props.baseUrl}api/audio/${this.state.zone.zoneId}/power/${power}`;
 
     const thing = this;
     fetch(url, {
@@ -183,34 +205,15 @@ class ZoneInformation extends React.Component {
         </Grid>
     );
 
-    let id = this.state.zone.zoneId + "SourceSelector";
-    let sourceVolume = (
-      <DropdownButton
-          id={id}
-          className="source-selector"
-          title={this.sourceName()}
-          value={this.state.sourceId}
-          onSelect={(eventKey) => {
-            this.sourceChanged(eventKey)
-          }}
-      >
-        {this.state.sources.map(source => {
-          return (
-              <MenuItem
-                  active={source.sourceId === this.state.sourceId}
-                  eventKey={source.sourceId}
-                  key={source.sourceId}
-                  value={source.sourceId}>
-                {source.name}
-              </MenuItem>)
-        })}
-      </DropdownButton>
-    );
-
     return (
         <Panel header={panelHeader}>
 
-          {sourceVolume}
+          <SourceSelector
+              zoneId={this.state.zone.zoneId}
+              selectedSourceId={this.state.sourceId}
+              sources={this.props.sources}
+              sourceChanged={this.sourceChanged}
+          />
 
           <ButtonGroup justified bsSize="large">
             <ButtonGroup justified bsSize="large">
@@ -225,16 +228,25 @@ class ZoneInformation extends React.Component {
   };
 }
 
+SourceSelector.propTypes = {
+  zoneId: PropTypes.number.isRequired, //
+  selectedSourceId: PropTypes.number, //
+  sources: PropTypes.arrayOf(PropTypes.shape({
+    name: PropTypes.string.isRequired, sourceId: PropTypes.number.isRequired
+  })).isRequired,
+  sourceChanged: PropTypes.func.isRequired
+};
+
 ZoneInformation.propTypes = {
   zoneInfo: PropTypes.shape({
     zone: PropTypes.shape({
       name: PropTypes.string.isRequired, zoneId: PropTypes.number.isRequired
-    }), //
+    }).isRequired, //
     source: PropTypes.shape({
-      name: PropTypes.string.isRequired, sourceId: PropTypes.number.isOptional
+      name: PropTypes.string.isRequired, sourceId: PropTypes.number.isRequired
     }), //
-    volume: PropTypes.string.isOptional, //
-    power: PropTypes.string.isOptional
+    volume: PropTypes.number, //
+    power: PropTypes.bool
   })
 };
 
